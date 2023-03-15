14993b11177f74436712d42b99b6aba0aa61e37b
Liang Xu <xl0418@gmail.com>
2023-03-15 15:37:56 -0700

    updated schemes code and doc

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# SinkingSchemes
+---
+title: "Several numerical schemes for interpolation of the sinking term in the oceanic models"
+author: 
+  - name: "Liang Xu"
+    url: https://github.com/jjallaire
+    affiliation: Global Micro Eco Group
+    affiliation_url: https://carnegiescience.edu/dr-emily-zakem
+---
+
+# The list of the schemes implemented here
+
+1. The central difference scheme;
+
+2. The first order upwind scheme;
+
+3. The first order upwind Godunov scheme;
+
+4. The second order upwind Godunov scheme;
+
+Schemes to be implemented:
+
+1. The third order (quadratic) upwind scheme (QUICK);
+
+2. The third order (quadratic) upwind Godunov scheme;
+
+3. The piecewise parabolic methods (PPM) scheme;
+
+etc.
+
+# The physical model used as the example
+
+The Burgers equation is used to demosntrate the performance difference in the schemes
+
+$$
+\frac{\partial u}{\partial t} +\frac{u}{2} \frac{\partial u}{\partial x} = 0
+$$
+
+where $u$ is the concentration of the tracer and the sinking rate is related to the concentration. 
+
+The concentration along x axis is initialized as a since shape. As such, at the middle place, there is a shock wave generated due to the rightward moving wave colliding with the leftward moving wave. The performance of schemes differ significantly right here due to large curve gradients that ruin the smootheness of the curve.
+
+# The central difference scheme
+
+
---
+++ main.py
@@ -0,0 +1,109 @@
+# a simple numberic solution for the burgers' equation: du/dt + du^2/2dx = 0
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from scipy.integrate import solve_ivp
+
+plt.style.use('_mpl-gallery')
+
+
+class burger_solver():
+    # Central difference scheme
+    def dudt_central(self, tt, u_bar):
+        dx = 1/len(u_bar)
+        f_bar = np.power(u_bar, 2)  / 2
+        f_interface = (f_bar[1:] + f_bar[:-1]) / 2
+        f_interface = np.insert(f_interface, 0 , 0)
+        f_interface = np.insert(f_interface, len(f_interface), 0)
+
+        du_bar_dt = (f_interface[:-1] - f_interface[1:]) / dx
+        return du_bar_dt
+
+    # First order upwind scheme
+    def dudt_firstorder_upwind(self, tt, u_bar):
+        dx = 1/len(u_bar)
+        f_bar = np.power(u_bar, 2)  / 2
+        slope_interface = (u_bar[1:] + u_bar[:-1]) / 2
+        f_interface = f_bar[:-1] * (slope_interface > 0) +f_bar[1:] * (slope_interface <= 0)
+        f_interface = np.insert(f_interface, 0 , 0)
+        f_interface = np.insert(f_interface, len(f_interface), 0)
+        du_bar_dt = (f_interface[:-1] - f_interface[1:]) / dx
+        return du_bar_dt
+
+
+    # First order Godunov scheme + central difference, taking the average volumn as the left and right flux value.
+    def dudt_Godunov_firstorder(self, tt, u_bar):
+        dx = 1 / len(u_bar)
+        f_bar = np.power(u_bar, 2) / 2
+        f_interface_max = np.maximum(f_bar[1:], f_bar[:-1])
+        f_interface_min = np.minimum(f_bar[1:], f_bar[:-1])
+        f_interface_min[np.where(np.logical_and(u_bar[1:] > 0, u_bar[:-1] < 0))  ] = 0
+        f_interface = f_interface_max * (u_bar[:-1] > u_bar[1:]) + f_interface_min * (u_bar[:-1] <= u_bar[1:])
+
+        f_interface = np.insert(f_interface, 0, 0)
+        f_interface = np.insert(f_interface, len(f_interface), 0)
+
+        du_bar_dt = (f_interface[:-1] - f_interface[1:]) / dx
+        return du_bar_dt
+
+    # Second order Godunov scheme.
+    def dudt_Godunov_upwind(self, tt, u_bar):
+        dx = 1 / len(u_bar)
+        f_bar = np.power(u_bar, 2) / 2
+        f_extro_bar = np.insert(f_bar, 0, 0)
+        f_extro_bar = np.insert(f_extro_bar, len(f_extro_bar), 0)
+
+        f_extro_left = np.zeros(len(u_bar)-1)
+        f_extro_right = np.zeros(len(u_bar)-1)
+
+        for ii in range(len(u_bar) - 1):
+            f_extro_left[ii] = f_extro_bar[ii+1] + (f_extro_bar[ii+1] - f_extro_bar[ii]) / (dx) * (dx / 2)
+            f_extro_right[ii] = f_extro_bar[ii + 2] - (f_extro_bar[ii + 3] - f_extro_bar[ii+2]) / (dx) * (dx / 2)
+
+        f_interface_max = np.maximum(f_extro_left, f_extro_right)
+        f_interface_min = np.minimum(f_extro_left, f_extro_right)
+        f_interface_min[np.where(np.logical_and(u_bar[1:] > 0, u_bar[:-1] < 0))  ] = 0
+        f_interface = f_interface_max * (u_bar[:-1] > u_bar[1:]) + f_interface_min * (u_bar[:-1] <= u_bar[1:])
+
+        f_interface = np.insert(f_interface, 0, 0)
+        f_interface = np.insert(f_interface, len(f_interface), 0)
+
+        du_bar_dt = (f_interface[:-1] - f_interface[1:]) / dx
+        return du_bar_dt
+
+    # Color gradient
+    def colorFader(self, c1, c2, mix=0):  # fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
+        c1 = np.array(mpl.colors.to_rgb(c1))
+        c2 = np.array(mpl.colors.to_rgb(c2))
+        return mpl.colors.to_hex((1 - mix) * c1 + mix * c2)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    x = np.linspace(0, 1, 100)
+    u_ini = np.sin(x * 2 * np.pi)
+
+    # fig1, ax2 = plt.subplots()
+    # ax2.plot(x, u_ini)
+    # plt.show()
+
+    bs = burger_solver()
+    bs.dudt_central(x, u_ini)
+
+    sol1 = solve_ivp(bs.dudt_firstorder_upwind, [0,1], u_ini, t_eval = [i/100 for i in range(100)])
+
+    c1 = 'red'  # blue
+    c2 = 'black'  # green
+    n = sol1.y.shape[1]
+
+    # plot
+    fig, ax = plt.subplots()
+    for nrow in range(sol1.y.shape[1]):
+        ax.plot(x, sol1.y[:, nrow], linewidth=2.0, color=bs.colorFader(c1,c2,nrow/n))
+
+    # ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+    #        ylim=(-1, 1), yticks=np.arange(-1, 1))
+
+    plt.show()
+
+
